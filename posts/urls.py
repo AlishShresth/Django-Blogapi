03e0c52a85c9f056860193969db0653bc46c84f9
Alish Shrestha <cresthaalish@gmail.com>
2025-02-05 20:49:05 +0545

    refact(posts): use viewsets, router

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import PostList, PostDetail
+# from .views import PostList, PostDetail, UserList, UserDetail
+from .views import UserViewSet, PostViewSet
 
+# urlpatterns = [
+#     path('', PostList.as_view(), name="post_list"),
+#     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
+#     path("users/", UserList.as_view(), name="user_list"),
+#     path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
+# ]
 
-urlpatterns = [
-    path('', PostList.as_view(), name="post_list"),
-    path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
-]
+router = SimpleRouter()
+router.register("users", UserViewSet, basename="users")
+router.register("", PostViewSet, basename="posts")
 
+urlpatterns = router.urls
